f8da57a6d7a51663c2605f2fefcc61ffccf2cfc4
joshdfg <slimmy0310@gmail.com>
2024-08-19 18:00:20 -0400

    feat: updates

---
+++ src/initializer.rs
@@ -0,0 +1,123 @@
+use {
+    crate::logic::Book,
+    crate::logic::BookType,
+    crate::logic::Role,
+    crate::logic::User,
+    std::io::{self, Write},
+};
+
+pub fn ititialize() {
+    let book1 = Book {
+        title: String::from("Zarahs deal"),
+        author: String::from("Robin Sharma"),
+        isbn: String::from("1232334"),
+        available: true,
+    };
+
+    let book2 = Book {
+        title: String::from("The rust book"),
+        author: String::from("Steve Klabnik and Carol Nichols"),
+        isbn: String::from("1234567"),
+        available: true,
+    };
+
+    // accessing associated functions
+    let bo = User::show_borrowed_book();
+    println!("{:?}", bo);
+
+    let mut user1 = User {
+        name: String::from("Joel"),
+        role: Role::Member("Joel".to_owned()),
+        borrowed_books: Vec::<Book>::new(),
+    };
+
+    let mut user2 = User {
+        name: String::from("Josh"),
+        role: Role::Librarian("Josh".to_owned()),
+        borrowed_books: Vec::<Book>::new(),
+    };
+
+    let mut books = vec![book1, book2];
+
+    println!("Enter the title of the book you want to borrow:");
+    io::stdout().flush().unwrap();
+    let mut book_title = String::new();
+    io::stdin()
+        .read_line(&mut book_title)
+        .expect("Failed to read line");
+
+    let book_to_borrow = books
+        .iter()
+        .position(|book| book.title == book_title.trim());
+
+    if let Some(index) = book_to_borrow {
+        let book = &mut books[index];
+        let book_type = match book.title.as_str() {
+            "Zarahs deal" => BookType::Fiction,
+            "The monk who sold his ferrari" => BookType::NonFiction,
+            "The rust book" => BookType::Reference,
+            _ => BookType::Magazine,
+        };
+        println!("Enter the name of the user who will borrow the book:");
+        io::stdout().flush().unwrap();
+        let mut borrower_name = String::new();
+        io::stdin()
+            .read_line(&mut borrower_name)
+            .expect("Failed to read line");
+
+        let borrower = match borrower_name.trim() {
+            "Joel" => &mut user1,
+            "Josh" => &mut user2,
+            _ => panic!("Invalid borrower name"),
+        };
+
+        borrower.borrow_book(book, book_type);
+        let borrowed_books = &borrower.show_borrowed_books();
+        println!("{:?}", borrowed_books);
+    } else {
+        println!(
+            "Sorry, we couldn't find a book titled '{}'. Please try again.",
+            book_title.trim()
+        );
+    }
+
+    println!("Enter the title of the book you want to return:");
+    io::stdout().flush().unwrap();
+    let mut book_title_return = String::new();
+    io::stdin()
+        .read_line(&mut book_title_return)
+        .expect("Failed to read line");
+
+    let book_to_return = books
+        .iter()
+        .position(|book| book.title == book_title_return.trim());
+
+    if let Some(index) = book_to_return {
+        let book = &mut books[index];
+        let book_type = match book.title.as_str() {
+            "Zarahs deal" => BookType::Fiction,
+            "The monk who sold his ferrari" => BookType::NonFiction,
+            "The rust book" => BookType::Reference,
+            _ => BookType::Magazine,
+        };
+        println!("Enter the name of the user who will return the book:");
+        io::stdout().flush().unwrap();
+        let mut returners_name = String::new();
+        io::stdin()
+            .read_line(&mut returners_name)
+            .expect("Failed to read line");
+
+        let returner = match returners_name.trim() {
+            "Joel" => &mut user1,
+            "Josh" => &mut user2,
+            _ => panic!("Invalid borrower name"),
+        };
+
+        returner.return_book(book, book_type);
+    } else {
+        println!(
+            "Sorry, we couldn't find a book titled '{}'. Please try again.",
+            book_title_return.trim()
+        );
+    }
+}
---
+++ src/logic.rs
@@ -0,0 +1,102 @@
+#[derive(Clone, Debug)]
+pub struct Book {
+    pub title: String,
+    pub author: String,
+    pub isbn: String,
+    pub available: bool,
+}
+
+pub enum Role {
+    Member(String),
+    Librarian(String),
+}
+
+#[derive(PartialEq)]
+pub enum BookType {
+    Fiction,
+    NonFiction,
+    Reference,
+    Magazine,
+}
+
+pub struct User {
+    pub name: String,
+    pub role: Role,
+    pub borrowed_books: Vec<Book>,
+}
+
+impl Book {
+    fn display(&self) {
+        println!(
+            "Title :{} Author:{} ISBN:{} Availability:{}",
+            self.title, self.author, self.isbn, self.available
+        );
+    }
+}
+
+impl User {
+    pub fn borrow_book(&mut self, book: &mut Book, book_type: BookType) {
+        match self.role {
+            Role::Member(_) => {
+                if book_type == BookType::Reference {
+                    println!("Members cant borrow reference types book")
+                } else if !book.available {
+                    println!("The requested book is not available");
+                } else {
+                    book.available = false;
+                    self.borrowed_books.push(book.clone());
+                    println!("{} borrowed {}", self.name, book.title);
+                }
+            }
+            Role::Librarian(_) => {
+                if book.available {
+                    book.available = false;
+                    self.borrowed_books.push(book.clone());
+                    println!("{} borrowed {}", self.name, book.title);
+                } else {
+                    println!("The requested book is not available");
+                }
+            }
+        }
+    }
+
+    pub fn show_borrowed_books(&self) -> Vec<Book> {
+        self.borrowed_books.to_owned()
+    }
+
+    //this is an associated function as it doesn't make reference to self and cannot be
+    // allowed to access self details because it doesn't take self as an argument!
+    pub fn show_borrowed_book() -> String {
+        String::from("Hello world!")
+    }
+
+    pub fn return_book(&mut self, book: &mut Book, book_type: BookType) {
+        match self.role {
+            Role::Member(_) => {
+                book.available = true;
+                let book_index = self
+                    .borrowed_books
+                    .iter()
+                    .position(|b| b.title == book.title);
+                if let Some(index) = book_index {
+                    self.borrowed_books.remove(index);
+                    println!("{} returned {}", self.name, book.title);
+                }
+                println!("not found");
+            }
+            Role::Librarian(_) => {
+                book.available = true;
+                let book_index = self
+                    .borrowed_books
+                    .iter()
+                    .position(|b| b.title == book.title);
+                if let Some(index) = book_index {
+                    self.borrowed_books.remove(index);
+                    println!("{} returned {}", self.name, book.title);
+                } else {
+                    println!("not founds")
+                }
+            }
+        }
+    }
+}
